a1800ae0a9b04f42cb92e5e4fb9138828f2de41b
Colossus FRC <colossusfrc@gmail.com>
2025-05-13 18:23:21 -0300

    Adição do lombok

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.ufpackages.CommandBased.CommandScheduler;
 
+
 public class Main extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
